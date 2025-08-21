d5c2aaf1b4f05f12c698be075cc4f38286bc23f9
dilyanaviktorova <dilqnahr18@gmail.com>
2025-08-21 17:04:59 +0300

    Add IdeaCenterExamPrep solution and project

---
+++ IdeaCenterExamPrep/IdeaCenterApiTests.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Authenticators;
+using IdeaCenterExamPrep.Models;
+
+
+namespace IdeaCenterExamPrep
+{
+    [TestFixture]
+    public class IdeaCenterApiTests
+    {
+        private RestClient client;
+        private static string? lastCreatedIdeaId;
+        private static string baseURL = "http://softuni-qa-loadbalancer-2137572849.eu-north-1.elb.amazonaws.com:84";
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            // Взимаме токен за достъп
+            string token = GetJwtToken("heartbeatsf@abv.bg", "82469314");
+
+            // Създаваме клиент с токен
+            var options = new RestClientOptions(baseURL)
+            {
+                Authenticator = new JwtAuthenticator(token)
+            };
+
+            client = new RestClient(options);
+        }
+
+        // Метод за логин и получаване на токен
+        private string GetJwtToken(string email, string password)
+        {
+            var loginClient = new RestClient(baseURL);
+
+            var request = new RestRequest("/api/User/Authentication", Method.Post);
+            request.AddJsonBody(new { email, password });
+
+            var response = loginClient.Execute(request);
+
+            var json = JsonSerializer.Deserialize<JsonElement>(response.Content);
+            return json.GetProperty("accessToken").GetString();
+        }
+        //All test here
+
+        [Order(1)]
+        [Test]
+        public void CreateIdea_WithRequiredFields_ShouldReturnSuccess()
+        {
+            var ideaRequest = new IdeaDTO
+            {
+                Title = "Test Idea",
+                Description = "This is a test idea description.",
+                Url = ""
+            };
+
+            var request = new RestRequest("/api/Idea/Create", Method.Post);
+            request.AddJsonBody(ideaRequest);
+            var response = this.client.Execute(request);
+            var createResponse = JsonSerializer.Deserialize<ApiResponseDTO>(response.Content);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(createResponse.Msg, Is.EqualTo("Successfully created!"));
+        }
+
+        [Order(2)]
+        [Test]
+        public void GetAllIdeas_ShouldReturnListOfIdeas()
+        {
+            var request = new RestRequest("/api/Idea/All", Method.Get);
+            var response = this.client.Execute(request);
+
+            var responsItems = JsonSerializer.Deserialize<List<ApiResponseDTO>>(response.Content);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(responsItems, Is.Not.Null);
+            Assert.That(responsItems, Is.Not.Empty);
+
+            lastCreatedIdeaId = responsItems.LastOrDefault()?.Id;
+        }
+
+        [Order(3)]
+        [Test]
+
+        public void EditExistingIdea_ShouldReturnSuccess()
+        {
+            var editRequest = new IdeaDTO
+            {
+                Title = "Edited Idea",
+                Description = "This is an updated test idea description.",
+                Url = ""
+            };
+
+            var request = new RestRequest($"/api/Idea/Edit",Method.Put);
+            request.AddQueryParameter("ideaId", lastCreatedIdeaId);
+            request.AddJsonBody(editRequest);
+            var response = this.client.Execute(request);
+            var editResponse = JsonSerializer.Deserialize<ApiResponseDTO>(response.Content);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(editResponse.Msg, Is.EqualTo("Edited successfully"));
+        }
+
+        [Order(4)]
+        [Test]
+        public void DeleteIdea_ShouldReturnSuccess()
+        {
+            var request = new RestRequest($"/api/Idea/Delete", Method.Delete);
+            request.AddQueryParameter("ideaId", lastCreatedIdeaId);
+            var response = this.client.Execute(request);
+            
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.Content, Does.Contain("The idea is deleted!"));
+        }
+
+        [Order(5)]
+        [Test]
+
+        public void CreateIdea_WithoutRequiredFields_ShouldReturnSuccessAgain()
+        {
+            var ideaRequest = new IdeaDTO
+            {
+                Title = "",
+                Description = ""
+            };
+
+            var request = new RestRequest("/api/Idea/Create", Method.Post);
+            request.AddJsonBody(ideaRequest);
+            var response = this.client.Execute(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+
+        }
+
+        [Order(6)]
+        [Test]
+
+        public void EditNonExistingIdea_ShouldReturnNotFound()
+        {
+            string nonExistingIdeaId = "123";
+            var editRequest = new IdeaDTO
+            {
+                Title = "Edited Non-Existing Idea",
+                Description = "This is an updated test idea description for a non-existing idea.",
+                Url = ""
+            };
+            var request = new RestRequest($"/api/Idea/Edit", Method.Put);
+            request.AddQueryParameter("ideaId", nonExistingIdeaId);
+            request.AddJsonBody(editRequest);
+            var response = this.client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+            Assert.That(response.Content, Does.Contain("There is no such idea!"));
+        }
+
+        [Order(7)]
+        [Test]
+
+        public void DeleteNonExistingIdea_ShouldReturnNotFound()
+        {
+            string nonExistingIdeaId = "123";
+            var request = new RestRequest($"/api/Idea/Delete", Method.Delete);
+            request.AddQueryParameter("ideaId", nonExistingIdeaId);
+            var response = this.client.Execute(request);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+            Assert.That(response.Content, Does.Contain("There is no such idea!"));
+        }
+
+        [OneTimeTearDown]
+        public void Teardown()
+        {
+        this.client?.Dispose();
+        }
+
+
+    }
+}
